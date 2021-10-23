7c2376716514927be784367c6c0c5aea2cece6d5
grify Sego <grifydev@gmail.com>
2021-10-23 23:38:41 +0200

    Initial commit

---
+++ main/yeggytfx.go
@@ -0,0 +1,50 @@
+components {
+  id: "sfx"
+  component: "/main/sfx.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "tfx"
+  component: "/main/tfx.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/yeggytfx.atlas\"\n"
+  "default_animation: \"tfx0\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
